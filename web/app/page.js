bae24afcc5d3136757a0b894fc9a0ff2dbbe0b7c
Firzus <lprieu@gaming.tech>
2023-11-23 18:55:40 +0100

    change port

--- web/app/page.js
+++ web/app/page.js
@@ -1,9 +1,21 @@
-import Image from 'next/image'
+async function getData() {
+  const urlApi = "http://localhost:3001/data.json";
+  const res = await fetch(urlApi, { cache: 'no-store' });
 
-export default function Home() {
+  if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
+  
   return (
     <main>
-      ok
+      app
     </main>
   )
 }
